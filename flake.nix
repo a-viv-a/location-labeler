19df7ebce0f641723665560c4113b592d02728cb
Aviva Ruben <aviva@rubenfamily.com>
2024-10-20 22:00:03 -0500

    denoflare v1w

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        denoflare-cli = "https://raw.githubusercontent.com/skymethod/denoflare/v0.6.0/cli/cli.ts";
       in
       {
         devShell =
@@ -21,6 +22,21 @@
           mkShell {
             buildInputs = [
               deno
+              # this is... not pure, but it works
+              (writeShellApplication {
+                name = "denoflare";
+                runtimeInputs = [ deno ];
+                text = ''
+                  ${lib.getExe deno} run \
+                  --unstable-worker-options \
+                  --allow-read \
+                  --allow-net \
+                  --allow-env \
+                  --allow-run \
+                  --allow-import \
+                  "${denoflare-cli}" "$@"
+                '';
+              })
             ];
           };
       }
