452470ab90390beb030087ed473eaef2682e5013
Aviva Ruben <aviva@rubenfamily.com>
2024-10-21 21:33:46 -0500

    call api

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,33 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+// https://nominatim.org/release-docs/latest/api/Output/
+type Place = {
+  "place_id": string,
+  "licence": string,
+  "osm_type": string,
+  "osm_id": string,
+  "boundingbox": [string, string, string, string],
+  "lat": string,
+  "lon": string,
+  // London, Greater London, England, SW1A 2DU, United Kingdom
+  "display_name": string,
+  "category": string,
+  "type": string,
+  "importance": number,
+  "place_rank": number,
+  "icon": string,
+  "address": {
+    "city": string,
+    "state_district": string,
+    "state": string,
+    "ISO3166-2-lvl4": string,
+    "postcode": string,
+    "country": string,
+    "country_code": string,
+  },
+};
+
 app.get('/', (c) => c.text("hiiiiii"))
 app.post('/request-label', async (c) => {
   const token = c.req.header('Token')
@@ -18,14 +45,31 @@ app.post('/request-label', async (c) => {
     return c.json({ msg: "missing Token header" })
   }
 
-  const latitude = c.req.query('lat')
-  const longitude = c.req.query('long')
-  if (latitude == undefined || longitude == undefined) {
+  const latitude_string = c.req.query('lat')
+  const longitude_string = c.req.query('lon')
+  if (latitude_string == undefined || longitude_string == undefined) {
     c.status(400)
     return c.json({ msg: "invalid / missing lat and long query params" })
   }
 
+  const latitude = parseFloat(latitude_string)
+  const longitude = parseFloat(longitude_string)
+
+  const headers = new Headers({
+    "User-Agent": "Bluesky Location Labeler"
+  })
+
+  // https://nominatim.org/release-docs/latest/api/Reverse/
+  const resp = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&accept-language=en&zoom=10&lat=${latitude}&lon=${longitude}`, {
+    "headers": headers,
+    "body": null,
+    "method": "GET"
+  });
+  console.log(resp.body)
+  const place = await resp.json() as Place;
+
   c.status(200)
+  console.log(place.address.city, place.address["ISO3166-2-lvl4"])
   return c.json({ location: "place" })
 })
 
