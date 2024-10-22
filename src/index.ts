d771dec5a8b1d9e76a0b44ba8c0bae99032cb906
Aviva Ruben <aviva@rubenfamily.com>
2024-10-21 21:10:00 -0500

    hmm

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,23 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get('/', (c) => c.text("hiiiiii"))
-app.get('/route', (c) => c.text("hello?"))
+app.post('/request-label', async (c) => {
+  const token = c.req.header('Token')
+
+  if (token == undefined || token.length == 0) {
+    c.status(401)
+    return c.json({ msg: "missing Token header" })
+  }
+
+  const latitude = c.req.query('lat')
+  const longitude = c.req.query('long')
+  if (latitude == undefined || longitude == undefined) {
+    c.status(400)
+    return c.json({ msg: "invalid / missing lat and long query params" })
+  }
+
+  c.status(200)
+  return c.json({ location: "place" })
+})
 
 export default app;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,20 +1,28 @@
-// test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { env } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import app from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+describe('request label', () => {
+  it('Should return 401 if missing token', async () => {
+    const res = await app.request('/request-label', {}, env)
 
-describe('Example', () => {
-  it('Should return 200 response', async () => {
-    const res = await app.request('/hello', {}, env)
-
-    expect(res.status).toBe(200)
-    expect(await res.json()).toEqual({
-      hello: 'world',
-      var: 'my variable',
-    })
+    expect(res.status).toBe(401)
   })
+  // it('Should return 400 if missing lat/long', async () => {
+  //   const seq = new Request('request-label', {
+  //     method: 'POST'
+  //   })
+  //   const res = await app.request(req)
+
+  //   expect(res.status).toBe(400)
+  // })
+  // it('Should return 200 response', async () => {
+  //   const res = await app.request('/request-label', {}, env)
+
+  //   expect(res.status).toBe(200)
+  //   // expect(await res.json()).toEqual({
+  //   //   hello: 'world',
+  //   //   var: 'my variable',
+  //   // })
+  // })
 })
